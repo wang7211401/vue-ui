2b547820d13353c66c85872456ddcb6a0165a804
wang7211401 <wang7211401@163.com>
2018-06-28 01:35:50 +0800

    parcel

--- src/button.vue
+++ src/button.vue
@@ -10,7 +10,7 @@
     .g-button{
         font-size: var(--font-size);
         height:var(--button-height);
-        background: var(--button-bg);
+        background:var(--button-bg);
         border-radius:var(--border-radius);
         border:1px solid var(--border-color);
         padding:0 1em;
