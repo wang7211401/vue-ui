bcab0dd34cc2315d4d2cc7a5daa452e8d1562337
wang7211401 <wang7211401@163.com>
2018-07-29 12:32:44 +0800

    添加loading 状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,8 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
+    @click="$emit('click')">
+        <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+        <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -10,6 +12,10 @@
     export default{
         props:{
             icon:{},
+            loading:{
+                type: Boolean,
+                default:false
+            },
             iconPosition:{
                 type:String,
                 default:'left',
@@ -21,6 +27,14 @@
     }
 </script>
 <style lang="scss">
+    @keyframes spin {
+        0%{
+            transform: rotate(0deg)
+        }
+        100%{
+            transform: rotate(360deg)
+        }
+    }
     .g-button{
         font-size: var(--font-size);
         height:var(--button-height);
@@ -53,11 +67,14 @@
             > .content{
                 order:1;
             }
-            > .icon{
+            > .g-icon{
                 order:2;
                 margin-right:0;
                 margin-left:.1em;
             }
+        }
+        .loading{
+            animation: spin 2s infinite linear;
         }
 
     }
