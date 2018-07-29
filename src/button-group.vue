f8ba37ac9c661eab3e587d34862812e0644a823a
wang7211401 <wang7211401@163.com>
2018-07-29 18:48:56 +0800

    安装 chai

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,38 +5,42 @@
 </template>
 <script>
 export default {
-    mounted(){
-        for(let node of this.$el.children){
-            console.log(node)
-            let name = node.nodeName.toLowerCase()
-            if(name !== 'button'){
-                console.warn(`g-button-group 的子元素应该全是 g-button，但是你写的是 ${name}`)
-            } 
-        }
+  mounted() {
+    for (let node of this.$el.children) {
+      console.log(node);
+      let name = node.nodeName.toLowerCase();
+      if (name !== "button") {
+        console.warn(
+          `g-button-group 的子元素应该全是 g-button，但是你写的是 ${name}`
+        );
+      }
     }
-}
+  }
+};
 </script>
 
 <style lang="scss">
-    .g-button-group{
-        display: inline-flex;
-        vertical-align: middle;
-        > .g-button{
-            border-radius: 0;
-            margin-left:-1px;
-            &:first-child{
-                border-top-left-radius: var(--border-radius);
-                border-bottom-left-radius: var(--border-radius);
-            }
-            &:last-child{
-                border-top-right-radius: var(--border-radius);
-                border-bottom-right-radius: var(--border-radius);
-            }
-            &:hover{
-                position: relative;
-                z-index:1;  
-            }
-        }
+.g-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  > .g-button {
+    border-radius: 0;
+    &:not(:first-child) {
+      margin-left: -1px;
     }
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
 </style>
 
