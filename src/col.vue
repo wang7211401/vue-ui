bf6b002e7b7937ac0fd6d93aaa81f655e23154f3
wang7211401 <wang7211401@163.com>
2018-08-03 00:31:40 +0800

    初步完成row和col

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name:'GvuiCol',
+    props:{
+        span:{
+            type:[Number,String]
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.col{
+    height:50px;
+    background: #378df1;
+    width:50%;
+    margin:10px 0;
+    $class-prefix:col-;
+    @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n}{
+            width:($n / 24) * 100%;
+        }
+    }
+}
+</style>
+
