c3de5b5c65f96e0ab04d93b19c3775bcac8039ad
wang7211401 <wang7211401@163.com>
2018-08-08 16:13:06 +0800

    基本功能完成

---
+++ src/sider.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="sider">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name:'GvuiSider'
+}
+</script>
+<style lang="scss" scoped>
+.sider{}
+</style>
