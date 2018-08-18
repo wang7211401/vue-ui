e6d85f62b06ba1132e26d8d7822bcc110a68cabc
wang7211401 <wang7211401@163.com>
2018-08-18 15:21:52 +0800

    只弹出一个toast

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ Vue.component('g-header', Header)
 Vue.component('g-content', Content)
 Vue.component('g-footer', Footer)
 Vue.component('g-sider', Sider)
-Vue.component('g-toast',Toast)
+Vue.component('g-toast', Toast)
 Vue.use(plugin)
 
 import createElement from 'vue'
@@ -40,22 +40,21 @@ new Vue({
         message: '123'
     },
     created() {
-        this.$toast('你的智商需要充值！', {
-            position: 'bottom',
-            enableHtml: false,
-            closeButton: {
-              text: '已充值',
-              callback () {
-                console.log('他说已经充值智商了')
-              }
-            },
-            autoClose: false,
-            autoCloseDelay: 3
-        })
     },
     methods: {
-       showToast(){
-        //    this.$toast('我是 message')
-       }
+        showToast() {
+            this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
+                position: 'middle',
+                enableHtml: false,
+                closeButton: {
+                    text: '已充值',
+                    callback() {
+                        console.log('他说已经充值智商了')
+                    }
+                },
+                autoClose: false,
+                autoCloseDelay: 3
+            })
+        }
     }
 })
