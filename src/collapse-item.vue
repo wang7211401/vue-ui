a1f9a7f8a53180f3b8464a99b081898894d67ec8
wang7211401 <wang7211401@163.com>
2018-09-09 23:21:50 +0800

    collapse 基本功能完成

---
+++ src/collapse-item.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="collapseItem">
+        <div class="title" @click="open = !open">
+            {{title}}
+        </div>
+        <div class="content" v-if="open">
+            <slot></slot>
+        </div>
+    </div> 
+</template>
+
+<script>
+export default {
+    name:'GvuiCollapseItem',
+    props:{
+        title:{
+            type:String,
+            required:true
+        }
+    },
+    data(){
+        return {
+            open:false
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    $grey:#ddd;
+    $border-radius:4px;
+    .collapseItem{
+        > .title{
+            border:1px solid $grey;
+            margin-top:-1px;
+            margin-left:-1px;
+            margin-right:-1px;
+            min-height:32px;
+            display: flex;
+            align-items: center;
+            padding:0 8px;
+        }
+        &:fisrt-child{
+            > .title{
+                border-top-left-radius: $border-radius;
+                border-bottom-left-radius: $border-radius
+            }
+        }
+        > .content{
+            padding:8px;
+        }
+    }
+</style>
+
