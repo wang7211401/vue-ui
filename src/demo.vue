34d3f0f275bad8fb3116ddce08982cb5a23a6b9d
wang7211401 <wang7211401@163.com>
2018-12-14 18:00:33 +0800

    添加 slides 和 slides-item

--- src/demo.vue
+++ src/demo.vue
@@ -1,105 +1,36 @@
 <template>
     <div id="app" style="padding:100px;">
-      <div style="padding:20px;">
-            <g-cascader :source.sync="source" popover-height="200px"
-            @update:source="onUpdateSource"
-            @update:selected="onUpdateSelected"
-            :selected.sync="selected" :load-data="loadData"></g-cascader>
-      </div>
-      <div style="padding-top: 16px;">
-        <h2>Toast</h2>
-        <p>
-          <strong>预览</strong>
-        </p>
-        <div>
-          <g-button @click="$toast('点击弹出提示')">上方弹出</g-button>
-          <g-button @click="$toast('点击弹出提示', {position:'middle'})">中间弹出</g-button>
-          <g-button @click="$toast('点击弹出提示', {position:'bottom'})">下方弹出</g-button>
-        </div>
-        <p>
-          <strong>代码</strong>
-        </p>
-      </div>
+      <g-slides class="wrapper" width="300px" height="200px" :selected.sync="selected">
+        <g-slides-item name="1">
+          <div class="box">1</div>
+        </g-slides-item>
+        <g-slides-item name="2">
+          <div class="box">2</div>
+        </g-slides-item>
+        <g-slides-item name="3">
+          <div class="box">3</div>
+        </g-slides-item>
+      </g-slides>
     </div>
 </template>
 <script>
-import Button from "./button";
-import Cascader from "./cascader";
-import plugin from './plugin'
-import db from "./db";
-import {removeListener} from './click-outside'
-import Vue from 'vue'
-Vue.use(plugin)
+import GSlides from "./slides";
+import GSlidesItem from "./slides-item";
 
-
-function ajax(parentId = 0) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let result = db.filter(item => item.parent_id == parentId);
-      result.forEach(node =>{
-        if(db.filter(item => item.parent_id === node.id).length > 0){
-          node.isLeaf = false
-        }else{
-          node.isLeaf = true
-        }
-      })
-      resolve(result);
-    }, 300);
-  });
-}
 
 export default {
   name: "demo",
   components: {
-    "g-button": Button,
-    "g-cascader": Cascader
+    GSlides,
+    GSlidesItem
   },
   data() {
     return {
-      selected: [],
-      // source: [
-      //   {
-      //     name: "浙江",
-      //     children: [
-      //       {
-      //         name: "杭州",
-      //         children: [{ name: "上城" }, { name: "下城" }, { name: "杭州" }]
-      //       },
-      //       {
-      //         name: "嘉兴",
-      //         children: [{ name: "南湖" }, { name: "秀洲" }, { name: "嘉善" }]
-      //       }
-      //     ]
-      //   },
-      //   {
-      //     name: "福建",
-      //     children: [
-      //       {
-      //         name: "福州",
-      //         children: [{ name: "鼓楼" }, { name: "台江" }, { name: "仓山" }]
-      //       }
-      //     ]
-      //   }
-      // ],
-      source: [],
+      selected: '2'
     };
   },
   created() {
-    ajax().then(result => {
-      this.source = result;
-    });
-  },
-  destroyed(){
-    removeListener()
-  },
-  methods: {
-    loadData({ id }, updateSource) {
-      ajax(id).then(result => {
-        updateSource(result);
-      }); 
-    },
-    onUpdateSource() {},
-    onUpdateSelected() {}
+   
   }
 };
 </script>
@@ -109,16 +40,13 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
-img {
-  max-width: 100%;
+.wrapper{
+  margin:40px;
 }
-html {
-  --font-size: 14px;
-}
-body {
-  font-size: var(--font-size);
-}
-.gulu-toast {
-  z-index: 30 !important;
+.box{
+  width:100%;
+  height:350px;
+  background: #ddd;
+  border:1px solid red;
 }
 </style> 
