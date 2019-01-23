088733913e523135b68536be77823597368c29a3
wang7211401 <wang7211401@163.com>
2019-01-23 11:24:20 +0800

    添加测试覆盖率

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,12 @@
 var webpackConfig = require('@vue/cli-service/webpack.config.js')
-module.exports = function (config) {
+module.exports = function(config) {
     config.set({
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
         // basePath: '',
-           // frameworks to use
-           // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-           frameworks: ['mocha'],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['mocha'],
         //    client: {
         //        chai: {
         //            includeStack: true
@@ -14,60 +14,67 @@ module.exports = function (config) {
         //    },
 
 
-           // list of files / patterns to load in the browser
-           files: [
-                'tests/**/*.spec.js'
-           ],
+        // list of files / patterns to load in the browser
+        files: [
+            'tests/**/*.spec.js'
+        ],
 
 
-           // list of files / patterns to exclude
+        // list of files / patterns to exclude
         //    exclude: [],
 
 
-           // preprocess matching files before serving them to the browser
-           // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-           preprocessors: {
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
             '**/*.spec.js': ['webpack', 'sourcemap']
-           },
+        },
 
 
-           // test results reporter to use
-           // possible values: 'dots', 'progress'
-           // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         //    reporters: ['progress'],
 
 
-           // web server port
+        // web server port
         //    port: 9876,
 
 
-           // enable / disable colors in the output (reporters and logs)
+        // enable / disable colors in the output (reporters and logs)
         //    colors: true,
 
 
-           // level of logging
-           // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         //    logLevel: config.LOG_INFO,
 
 
-           // enable / disable watching file and executing tests whenever any file changes
+        // enable / disable watching file and executing tests whenever any file changes
         //    autoWatch: true,
 
-           webpack: webpackConfig,
+        webpack: webpackConfig,
 
-           reporters: ['spec'],
-           autoWatch: true,
-           // start these browsers
-           // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-           browsers: ['ChromeHeadless'],
+        reporters: ['spec', 'coverage'],
+        coverageReporter: {
+            dir: './coverage',
+            reporters: [
+                { type: 'lcov', subdir: '.' },
+                { type: 'text-summary' }
+            ]
+        },
+        autoWatch: true,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['ChromeHeadless'],
 
 
-           // Continuous Integration mode
-           // if true, Karma captures browsers, runs the tests and exits
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
         //    singleRun: false,
 
-           // Concurrency level
-           // how many browser should be started simultaneous
+        // Concurrency level
+        // how many browser should be started simultaneous
         //    concurrency: Infinity
-       })
-   }
+    })
+}
